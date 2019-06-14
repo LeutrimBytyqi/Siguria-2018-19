9058500b50b25dff77fa9e2aa9aff871a4dba07c
Leutrim <leutrim.bytyqi@studentet.uni-pr.edu>
2019-06-14 21:21:28 +0200

    ClientUDP
    @guximselmani @hajrijemjeku

---
+++ SiguriV1.1Gr_22/SiguriV1.1/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace SiguriV1._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Client = new System.Windows.Forms.Button();
+            this.Server = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Client
+            // 
+            this.Client.Location = new System.Drawing.Point(260, 63);
+            this.Client.Name = "Client";
+            this.Client.Size = new System.Drawing.Size(292, 97);
+            this.Client.TabIndex = 0;
+            this.Client.Text = "Client";
+            this.Client.UseVisualStyleBackColor = true;
+            this.Client.Click += new System.EventHandler(this.Client_Click);
+            // 
+            // Server
+            // 
+            this.Server.Location = new System.Drawing.Point(260, 213);
+            this.Server.Name = "Server";
+            this.Server.Size = new System.Drawing.Size(292, 113);
+            this.Server.TabIndex = 1;
+            this.Server.Text = "Server";
+            this.Server.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Server);
+            this.Controls.Add(this.Client);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Client;
+        private System.Windows.Forms.Button Server;
+    }
+}
+
