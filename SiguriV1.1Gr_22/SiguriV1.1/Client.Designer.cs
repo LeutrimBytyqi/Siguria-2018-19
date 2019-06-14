08f561fb6b0451bf4f92f228b3799bb26de99122
Hajrije <44509293+Hajrije@users.noreply.github.com>
2019-06-14 23:03:20 +0200

    formClient

--- SiguriV1.1Gr_22/SiguriV1.1/Client.Designer.cs
+++ SiguriV1.1Gr_22/SiguriV1.1/Client.Designer.cs
@@ -28,12 +28,268 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.connectBtn = new System.Windows.Forms.Button();
+            this.signUpBtn = new System.Windows.Forms.Button();
+            this.signInBtn = new System.Windows.Forms.Button();
+            this.hostClientLbl = new System.Windows.Forms.Label();
+            this.portLbl = new System.Windows.Forms.Label();
+            this.hostClientTxt = new System.Windows.Forms.TextBox();
+            this.portClientTxt = new System.Windows.Forms.TextBox();
+            this.messageClientTxt = new System.Windows.Forms.TextBox();
+            this.signUpLbl = new System.Windows.Forms.Label();
+            this.signInLbl = new System.Windows.Forms.Label();
+            this.nameLbl = new System.Windows.Forms.Label();
+            this.surnameLbl = new System.Windows.Forms.Label();
+            this.emLbl = new System.Windows.Forms.Label();
+            this.pasLbl = new System.Windows.Forms.Label();
+            this.emailLbl = new System.Windows.Forms.Label();
+            this.passwordLbl = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // connectBtn
+            // 
+            this.connectBtn.Location = new System.Drawing.Point(674, 12);
+            this.connectBtn.Name = "connectBtn";
+            this.connectBtn.Size = new System.Drawing.Size(111, 51);
+            this.connectBtn.TabIndex = 0;
+            this.connectBtn.Text = "Connect";
+            this.connectBtn.UseVisualStyleBackColor = true;
+            // 
+            // signUpBtn
+            // 
+            this.signUpBtn.Location = new System.Drawing.Point(135, 416);
+            this.signUpBtn.Name = "signUpBtn";
+            this.signUpBtn.Size = new System.Drawing.Size(75, 23);
+            this.signUpBtn.TabIndex = 1;
+            this.signUpBtn.Text = "Sign Up";
+            this.signUpBtn.UseVisualStyleBackColor = true;
+            // 
+            // signInBtn
+            // 
+            this.signInBtn.Location = new System.Drawing.Point(515, 339);
+            this.signInBtn.Name = "signInBtn";
+            this.signInBtn.Size = new System.Drawing.Size(75, 23);
+            this.signInBtn.TabIndex = 2;
+            this.signInBtn.Text = "Sign In";
+            this.signInBtn.UseVisualStyleBackColor = true;
+            // 
+            // hostClientLbl
+            // 
+            this.hostClientLbl.AutoSize = true;
+            this.hostClientLbl.Location = new System.Drawing.Point(35, 26);
+            this.hostClientLbl.Name = "hostClientLbl";
+            this.hostClientLbl.Size = new System.Drawing.Size(45, 17);
+            this.hostClientLbl.TabIndex = 3;
+            this.hostClientLbl.Text = "Host :";
+            // 
+            // portLbl
+            // 
+            this.portLbl.AutoSize = true;
+            this.portLbl.Location = new System.Drawing.Point(362, 26);
+            this.portLbl.Name = "portLbl";
+            this.portLbl.Size = new System.Drawing.Size(42, 17);
+            this.portLbl.TabIndex = 4;
+            this.portLbl.Text = "Port :";
+            // 
+            // hostClientTxt
+            // 
+            this.hostClientTxt.Location = new System.Drawing.Point(113, 26);
+            this.hostClientTxt.Name = "hostClientTxt";
+            this.hostClientTxt.Size = new System.Drawing.Size(201, 22);
+            this.hostClientTxt.TabIndex = 5;
+            // 
+            // portClientTxt
+            // 
+            this.portClientTxt.Location = new System.Drawing.Point(440, 26);
+            this.portClientTxt.Name = "portClientTxt";
+            this.portClientTxt.Size = new System.Drawing.Size(194, 22);
+            this.portClientTxt.TabIndex = 6;
+            // 
+            // messageClientTxt
+            // 
+            this.messageClientTxt.Location = new System.Drawing.Point(708, 102);
+            this.messageClientTxt.Multiline = true;
+            this.messageClientTxt.Name = "messageClientTxt";
+            this.messageClientTxt.Size = new System.Drawing.Size(297, 311);
+            this.messageClientTxt.TabIndex = 7;
+            // 
+            // signUpLbl
+            // 
+            this.signUpLbl.AutoSize = true;
+            this.signUpLbl.Location = new System.Drawing.Point(27, 125);
+            this.signUpLbl.Name = "signUpLbl";
+            this.signUpLbl.Size = new System.Drawing.Size(240, 17);
+            this.signUpLbl.TabIndex = 8;
+            this.signUpLbl.Text = "Sign Up if you don\'t have an account";
+            // 
+            // signInLbl
+            // 
+            this.signInLbl.AutoSize = true;
+            this.signInLbl.Location = new System.Drawing.Point(361, 125);
+            this.signInLbl.Name = "signInLbl";
+            this.signInLbl.Size = new System.Drawing.Size(249, 17);
+            this.signInLbl.TabIndex = 9;
+            this.signInLbl.Text = "Sign In if you already have an account";
+            // 
+            // nameLbl
+            // 
+            this.nameLbl.AutoSize = true;
+            this.nameLbl.Location = new System.Drawing.Point(4, 201);
+            this.nameLbl.Name = "nameLbl";
+            this.nameLbl.Size = new System.Drawing.Size(53, 17);
+            this.nameLbl.TabIndex = 10;
+            this.nameLbl.Text = "Name :";
+            // 
+            // surnameLbl
+            // 
+            this.surnameLbl.AutoSize = true;
+            this.surnameLbl.Location = new System.Drawing.Point(4, 261);
+            this.surnameLbl.Name = "surnameLbl";
+            this.surnameLbl.Size = new System.Drawing.Size(73, 17);
+            this.surnameLbl.TabIndex = 11;
+            this.surnameLbl.Text = "Surname :";
+            // 
+            // emLbl
+            // 
+            this.emLbl.AutoSize = true;
+            this.emLbl.Location = new System.Drawing.Point(4, 313);
+            this.emLbl.Name = "emLbl";
+            this.emLbl.Size = new System.Drawing.Size(50, 17);
+            this.emLbl.TabIndex = 12;
+            this.emLbl.Text = "Email :";
+            // 
+            // pasLbl
+            // 
+            this.pasLbl.AutoSize = true;
+            this.pasLbl.Location = new System.Drawing.Point(4, 366);
+            this.pasLbl.Name = "pasLbl";
+            this.pasLbl.Size = new System.Drawing.Size(77, 17);
+            this.pasLbl.TabIndex = 13;
+            this.pasLbl.Text = "Password :";
+            // 
+            // emailLbl
+            // 
+            this.emailLbl.AutoSize = true;
+            this.emailLbl.Location = new System.Drawing.Point(368, 185);
+            this.emailLbl.Name = "emailLbl";
+            this.emailLbl.Size = new System.Drawing.Size(50, 17);
+            this.emailLbl.TabIndex = 14;
+            this.emailLbl.Text = "Email :";
+            // 
+            // passwordLbl
+            // 
+            this.passwordLbl.AutoSize = true;
+            this.passwordLbl.Location = new System.Drawing.Point(368, 286);
+            this.passwordLbl.Name = "passwordLbl";
+            this.passwordLbl.Size = new System.Drawing.Size(77, 17);
+            this.passwordLbl.TabIndex = 15;
+            this.passwordLbl.Text = "Password :";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(94, 195);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(173, 22);
+            this.textBox3.TabIndex = 16;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(94, 256);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(173, 22);
+            this.textBox4.TabIndex = 17;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(94, 308);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(173, 22);
+            this.textBox5.TabIndex = 18;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(94, 366);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(173, 22);
+            this.textBox6.TabIndex = 19;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(460, 185);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(174, 22);
+            this.textBox7.TabIndex = 20;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(460, 283);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(174, 22);
+            this.textBox8.TabIndex = 21;
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1017, 487);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.passwordLbl);
+            this.Controls.Add(this.emailLbl);
+            this.Controls.Add(this.pasLbl);
+            this.Controls.Add(this.emLbl);
+            this.Controls.Add(this.surnameLbl);
+            this.Controls.Add(this.nameLbl);
+            this.Controls.Add(this.signInLbl);
+            this.Controls.Add(this.signUpLbl);
+            this.Controls.Add(this.messageClientTxt);
+            this.Controls.Add(this.portClientTxt);
+            this.Controls.Add(this.hostClientTxt);
+            this.Controls.Add(this.portLbl);
+            this.Controls.Add(this.hostClientLbl);
+            this.Controls.Add(this.signInBtn);
+            this.Controls.Add(this.signUpBtn);
+            this.Controls.Add(this.connectBtn);
+            this.Name = "Client";
             this.Text = "Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button connectBtn;
+        private System.Windows.Forms.Button signUpBtn;
+        private System.Windows.Forms.Button signInBtn;
+        private System.Windows.Forms.Label hostClientLbl;
+        private System.Windows.Forms.Label portLbl;
+        private System.Windows.Forms.TextBox hostClientTxt;
+        private System.Windows.Forms.TextBox portClientTxt;
+        private System.Windows.Forms.TextBox messageClientTxt;
+        private System.Windows.Forms.Label signUpLbl;
+        private System.Windows.Forms.Label signInLbl;
+        private System.Windows.Forms.Label nameLbl;
+        private System.Windows.Forms.Label surnameLbl;
+        private System.Windows.Forms.Label emLbl;
+        private System.Windows.Forms.Label pasLbl;
+        private System.Windows.Forms.Label emailLbl;
+        private System.Windows.Forms.Label passwordLbl;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
     }
 }
